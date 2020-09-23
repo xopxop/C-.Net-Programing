9f71fd5b0145ded20a6f96dd1882c87f443e2b0f
Van Du Than <dthan@Vans-MacBook-Pro.local>
2020-09-24 01:49:39 +0300

    Added SchoolTracker

---
+++ 1_Intro_to_.NET/SchoolTracker/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace SchoolTracker
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var studentGrade = new int[] {88 , 77, 42, 21, 77, 88, 99, 100, 24, 68};
+
+            foreach(var item in studentGrade)
+            {
+                Console.WriteLine(item);
+            }
+            
+        }
+    }
+}
